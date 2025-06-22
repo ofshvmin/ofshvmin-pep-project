7784c44637299c38b4204858dd920fdac1ba54b6
Daniel Mathews <daniel.g.mathews@gmail.com>
2025-06-22 20:44:14 +0000

    debug AccountService constructor and  addAccount method

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -6,28 +6,27 @@ import DAO.AccountDAO;
 import java.util.List;
 
 public class AccountService {
-    public AccountDAO accountDAO;
+    private AccountDAO accountDAO;
     
     public AccountService() {
-        accountDAO = new AccountDAO();
-    
-        public AccountService(AccountDAO accountDAO) {
-            this.accountDAO = accountDAO;
-        }
-
-        public Account addAccount(Account account) {
-            List<Account> existingAccounts = accountDAO.getAllAccounts(); //< - doesn'ty exist yet
-            for(Account existingAccount : existingAccounts) {
-                if(existingAccount.getUsername().equalsIgnoreCase(account.getUsername())) {
-                    //what actuall??
-                };
-            }
-
-            
-        }
-
-
+        this.accountDAO = new AccountDAO();
     }
-}
 
+
+    public AccountService(AccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
+    }
+
+    public Account addAccount(Account account) {
+        // List<Account> existingAccounts = accountDAO.getAllAccounts(); //< - doesn'ty exist yet
+        // for(Account existingAccount : existingAccounts) {
+        //     if(existingAccount.getUsername().equalsIgnoreCase(account.getUsername())) {
+        //         //what actuall??
+        //         return null;
+        //     }
+        // }
+        return accountDAO.insertAccount(account);
+    }
+    
+}
     

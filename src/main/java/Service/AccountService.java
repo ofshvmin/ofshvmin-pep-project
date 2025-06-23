19ac2fb5c51d08754e72dd34c7bec4c62c3b299d
Daniel Mathews <daniel.g.mathews@gmail.com>
2025-06-23 14:44:11 +0000

    add password and username validations to accountService method

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -5,6 +5,7 @@ import DAO.AccountDAO;
 
 import java.util.List;
 
+
 public class AccountService {
     private AccountDAO accountDAO;
     
@@ -12,16 +13,24 @@ public class AccountService {
         this.accountDAO = new AccountDAO();
     }
 
-
     public AccountService(AccountDAO accountDAO) {
         this.accountDAO = accountDAO;
     }
 
+    public List<Account> getAllAccounts() {
+        return accountDAO.getAllAccounts();
+    }
+
     public Account addAccount(Account account) {
-        List<Account> existingAccounts = accountDAO.getAllAccounts(); //< - doesn'ty exist yet
+        List<Account> existingAccounts = accountDAO.getAllAccounts();
         for(Account existingAccount : existingAccounts) {
             if(existingAccount.getUsername().equalsIgnoreCase(account.getUsername())) {
-                //what actuall??
+                return null;
+            }
+            if(account.getPassword().length() < 4) {
+                return null;
+            }
+            if(account.getUsername().length() < 1) {
                 return null;
             }
         }
@@ -29,4 +38,3 @@ public class AccountService {
     }
     
 }
-    

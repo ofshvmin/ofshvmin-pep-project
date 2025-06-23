674aa3bc950e512c221de97934100a3803d94840
Daniel Mathews <daniel.g.mathews@gmail.com>
2025-06-23 14:47:38 +0000

    degub insertAccount method

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -3,31 +3,22 @@ package DAO;
 import Util.ConnectionUtil;
 import Model.Account;
 
-// import java.sql.ResultSet;
-// import java.sql.SQLException;
-// import java.util.ArrayList;
-// import java.util.List;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 
-
-
-
 public class AccountDAO {
-
 
     public List<Account> getAllAccounts(){
         Connection connection = ConnectionUtil.getConnection();
         List<Account> accounts = new ArrayList<>();
         try {
-            //Write SQL logic here
             String sql = "SELECT * FROM Account";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
-                Account account = new Account(rs.getInt("user_id"), rs.getString("username"), rs.getString("password"));
+                Account account = new Account(rs.getInt("account_id"), rs.getString("username"), rs.getString("password"));
                 accounts.add(account);
             }
         }catch(SQLException e){
@@ -45,11 +36,15 @@ public class AccountDAO {
             preparedStatement.setString(1, account.getUsername());
             preparedStatement.setString(2, account.getPassword());
 
-
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pkeyResultSet.next()) {
+                int generated_account_id = (int) pkeyResultSet.getInt(1);
+                return new Account(generated_account_id, account.getUsername(), account.getPassword()); //<-----what is actual method name?  Why does Library app say 'getLong'?
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return null;
     }
-    
 }
